9e80f229f3e22c61432705f2f61ecc0a63a16969
Antonio Vinicius <101371607+avinicius19@users.noreply.github.com>
2022-08-11 22:12:41 -0300

    Add files via upload

---
+++ Aula 01/Aula01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{  
+            Console.WriteLine("Informe uma largura:");
+            double largura = Convert.ToDouble(Console.ReadLine());       
+            Console.WriteLine("Informe uma altura:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double area = CalcularArea(largura, altura );
+            Console.WriteLine("A area equivale a:" +  area);
+            //Console.WriteLine(area);     
+            }
+            catch(Exception erro) {
+                Console.WriteLine(erro.Message);
+            }   
+        }
+        static double CalcularArea(double largura, double altura){
+            return largura * altura;
+            
+        }
+    }
+    
+}
+
